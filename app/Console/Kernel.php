826a297eb315f7141ebae52376889ac69a9058e4
Yaroslav Melnychenko <jaroslav.melnychenko@gmail.com>
2023-11-20 13:31:30 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('telescope:prune')->daily();
-        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
